79061ae74701b7ddd93e9edb988eb2173cfc7c1a
Yun <dudlf016@gmail.com>
2023-03-06 14:23:51 +0900

    type: fix allPromisesObj type
    Awiated - ts 4.5

--- src/utils/fp/allPromisesObj/index.d.ts
+++ src/utils/fp/allPromisesObj/index.d.ts
@@ -1,10 +1,7 @@
-// @flow
-
 import { Key } from 'react'
-import { $ObjMap } from '../../../types'
 
-type UnpackPromise = <T>(promise: Promise<T>) => T
-
-export default function allPromisesObj<T, Key, Spec = { [key: string | number]: Promise<T> }>(
+export default function allPromisesObj<T, Spec = { [key: Key]: Promise<T> }>(
   promisesObj: Spec,
-): Promise<$ObjMap<Spec, UnpackPromise>>
+): Promise<{
+  [K in keyof Spec]: Awaited<Spec[K]>
+}>
