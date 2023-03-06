38dd120cdaec339b2fc9888183477459212a643b
Yun <dudlf016@gmail.com>
2023-03-06 14:18:16 +0900

    type: fix return type of src/utils/fp/keys

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,10 +4,6 @@ export type $NonMaybeType<T> = T
 
 export type $ObjMap<O, T> = { [K in keyof O]: T }
 
-export type $Keys<Type> = { k: keyof Type }
-
-export type Array<Type> = Type[]
-
 // TODO: FIX TS
 export type $Call<F, T> = any
 
--- src/utils/fp/keys/index.d.ts
+++ src/utils/fp/keys/index.d.ts
@@ -1,5 +1,1 @@
-// @flow
-
-import { $Keys, Array } from '../../../types'
-
-export default function keys<T = {}>(obj: T): Array<$Keys<T>>
+export default function keys<T = {}>(obj: T): (keyof T)[]
